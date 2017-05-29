18ba3d4cdb90323bbeab2b6a0df6c948fc0d010c
TanK-PC\TanK <ezequiel.vannucci@gmail.com>
2017-05-29 19:01:48 -0300

    Minor changes

---
+++ 01-JS-DOM-APIs/js/javascript.js
@@ -0,0 +1,12 @@
+function loadpage()
+{
+	var pageLoaded = document.getElementById('load');
+	pageLoaded.style.opacity="1";
+	pageLoaded.style.transition="4000ms all ease";
+}
+
+function clickme()
+{
+	alert("Ouch, You clicked me!");
+
+}
