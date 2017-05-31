1f1f58b7580a99991eecb11123578688590f6322
TanK-PC\TanK <ezequiel.vannucci@gmail.com>
2017-05-30 22:04:08 -0300

    Topic 1 Exercise 3 first try

--- 01-JS-DOM-APIs/js/javascript.js
+++ 01-JS-DOM-APIs/js/javascript.js
@@ -10,3 +10,23 @@ function clickme()
 	alert("Ouch, You clicked me!");
 
 }
+
+
+/*var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+       // Typical action to be performed when the document is ready:
+       document.getElementById("joke").innerHTML = xhttp.responseText;
+    }
+};
+xhttp.open("GET", "http://api.icndb.com/jokes/random", true);
+xhttp.send();*/
+
+
+
+function jokeFunction(){
+	var req = new XMLHttpRequest();
+		req.open('GET', 'http://api.icndb.com/jokes/random/', true);
+		document.getElementById("joke").innerHTML = req.responseText;
+		console.log(req.responseText);
+}
