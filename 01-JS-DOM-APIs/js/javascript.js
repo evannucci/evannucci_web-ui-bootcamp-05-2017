4230a818ae061e8067d76cb29bffffbfe76e5e23
TanK-PC\TanK <ezequiel.vannucci@gmail.com>
2017-06-07 22:25:53 -0300

    Topic1 Exercise3

--- 01-JS-DOM-APIs/js/javascript.js
+++ 01-JS-DOM-APIs/js/javascript.js
@@ -5,38 +5,72 @@ function loadPage()
 	pageLoaded.style.transition="4000ms all ease";
 }
 
+ //Exercise 3
+function one (){
 
-
-
-function jokeMe(config)
-{
-    let config = {
-    methodType: 'GET',
-    responseType: 'JSON',
-    url: 'http://api.icndb.com/jokes/random'
-}
-    let promiseObj = new Promise(function(resolve, reject)
-    {        
-      
-        let xhr = new XMLHttpRequest();
-        xhr.open(config.methodType, config.url, true);
-        xhr.responseType = config.responseType;
-
-        xhr.addEventListener('load', function(response)
-        {
-            if (response.currentTarget.status === 200)
-            {
-                resolve(response.currentTarget.response.value.joke);
-            }
-            else
-            {
-                reject('Fail Request');
-            }
+(function() {
+    'use strict';
+    function promiseJoke(config) {
+        let promise = new Promise(function(resolve, reject) {
+            let request = new XMLHttpRequest();
+            request.open(config.methodType, config.url, true);
+            request.responseType = config.responseType;
+            request.addEventListener('load', function(response) {
+                if (response.currentTarget.status === 200) {
+                    resolve(response.currentTarget.response.value.joke);
+                } else {
+                    let turnred = document.getElementById('joke');
+                    turnred.color = "#FF0000";
+                    reject('Bad request');
+                }
+            });
+            request.send(null);
         });
-            xhr.send(null);
-    }
         return promise;
+    };
+
+    function addJoke(joke, text) {
+        let e = document.getElementById('joke');
+        e.innerHTML = text;
+    }
+    let config = {
+        methodType: 'GET',
+        responseType: 'json',
+        url: 'http://api.icndb.com/jokes/random'
+    };
+
+    promiseJoke(config).then(
+        // promise ok
+        function(response) {
+            console.log('OK => ', response);
+            addJoke('target', response);
+        },
+        // promise bad
+        function(response) {
+            console.log('BAD => ', response);
+        }
+    );
+}) ();
+
 }
 
+
+
+
+function createTable(){
+
+const matrix = [
+    ["Jhon Jackson", "21 years"],
+    ["Jack Jhonson", "31 years"],
+    ["Peter White", "55 years"],
+    ["Walter Red", "33 years"]
+];
+
+var newTable = document.createElemente("table")
+for (i=0 ; i<matrix.length ; i++){
+
+}
+   
+}
 
 
